75ba8782fdf4211b11b69be94b49d6970caf2864
henny <kimhyein1105@gmail.com>
2023-09-24 12:30:11 +0900

    리액트 초기 셋팅

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Footer } from './components/common/Footer';
+import Header from './components/common/Header';
+import Modal from './components/common/Modal';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Footer />
+			<Modal />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,9 @@
+const Modal = () => {
+	return (
+		<aside>
+			<h1>modal</h1>
+		</aside>
+	);
+};
+
+export default Modal;
